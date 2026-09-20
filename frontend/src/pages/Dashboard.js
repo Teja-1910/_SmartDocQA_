5418199e59cfc1df9c85b3a41cf99ccdb683c368
Brahma Teja Reddy Polu <brahmateja1910@gmail.com>
2026-09-20 18:06:02 +0530

    Update API endpoint for fetching data

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -41,7 +41,7 @@ export default function Dashboard({ user, onLogout }) {
 ]);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/query", {
+      const res = await fetch("smartdocqa-production.up.railway.app/query", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
